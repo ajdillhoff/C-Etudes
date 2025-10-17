707934c5cb0378c09ce2cb3e842c519f18359680
Alex Dillhoff <ajdillhoff@gmail.com>
2025-10-17 11:53:00 -0500

    Updated solution.

--- 8-roll_dice/solution/rolldice.c
+++ 8-roll_dice/solution/rolldice.c
@@ -5,20 +5,42 @@
 
 #define BUFSIZE 1024
 
-int rolld6() {
-    return rand() % 6 + 1;
+int roll(int num_die, int die_size) {
+    int sum = 0;
+
+    for (int i = 0; i < num_die; i++) {
+        sum += rand() % die_size + 1;
+    }
+    return sum;
 }
 
 int main() {
+
+    srand(time(NULL));
+
     char buffer[BUFSIZE] = { 0 };
-    printf("Press ENTER to roll. Input Q to quit.");
+    int num_die = 0;
+    int die_size = 0;
+    printf("Enter roll. Input Q to quit.");
 
     while (fgets(buffer, BUFSIZE, stdin)) {
+        buffer[strcspn(buffer, "\n")] = 0;
+
         if (buffer[0] == 'Q') {
             break;
         }
 
-        printf("Rolling d6: %d\n", rolld6());
+        if (buffer[0] == 'd') {
+            num_die = 1;
+            die_size = atoi(strtok(buffer, "d"));
+        } else {
+            num_die = atoi(strtok(buffer, "d"));
+            die_size = atoi(strtok(NULL, "d"));
+        }
+
+        // Parse input 10d100
+
+        printf("Rolling %dd%d: %d\n", num_die, die_size, roll(num_die, die_size));
     }
 
     printf("Good bye! Nice to know you.\n");
